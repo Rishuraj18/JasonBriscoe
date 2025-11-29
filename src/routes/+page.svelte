e2297a333a4715ad20fd65965f874881c238a8d9
RishuRaj18 <rrishuraj12345678@gmail.com>
2025-11-30 01:46:23 +0530

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,9 @@
 <script>
     
 
-    // @ts-ignore
     import Landingfolio from  '$lib/components/Landingfolio.svelte';
-    // import Credit from "../lib/components/Credit.svelte";
-    // import '../app.css';
-// import Springy from "$lib/components/Springy.svelte";
+    
 </script>
-<!-- <Credit/> -->
+
 <Landingfolio />
- <!-- <Springy/> -->
- <!-- <script>
-    import { fly } from 'svelte/transition';
-    let items = ['Item 1', 'Item 2', 'Item 3'];
-  </script>
-  
-  <ul>
-    {#each items as item (item)}
-      <li transition:fly={{ x: 20, duration: 400 }}>{item}</li>
-    {/each}
-  </ul> -->
-  
+ 
